bdbe573d6816fcdab26eaa99194b57a281801c25
Munir <munirkun123@gmail.com>
2023-03-20 09:13:09 +0700

    add some dependencies

---
+++ models/chili.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Chili extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Chili.init({
+    name: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Chili',
+  });
+  return Chili;
+};
